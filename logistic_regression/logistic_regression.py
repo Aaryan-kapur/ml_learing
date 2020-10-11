2ba348dfc1f1e21607a2e663c1238a334e374991
ashok-mbp16 <ashok_a380@icloud.com>
2020-10-11 12:03:32 +0530

    logitic regression with confusion matrix digit dataset

--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -13,11 +13,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.figure(figsize=(20,4))
 for index, (image, label) in enumerate(zip(digits.data[0:5], digits.target[0:5])):
- plt.subplot(1, 5, index + 1)
- plt.imshow(np.reshape(image, (8,8)), cmap=plt.cm.gray)
- plt.title('Training: %i\n' % label, fontsize = 20)
+    plt.subplot(1, 5, index + 1)
+    plt.imshow(np.reshape(image, (8,8)), cmap=plt.cm.gray)
+    plt.title('Training: %i\n' % label, fontsize = 20)
 
- from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.25, random_state=0)
 
 #imprt logistic regression
